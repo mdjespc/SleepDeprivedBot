821ff05181e8a086553e3033ea6db0cf1f30a2bc
mdjespc <mdjespc@gmail.com>
2025-04-23 11:37:08 -0400

    Implement /mute and /unmute

--- Modules/SlashCommands/AdminSlashModule.cs
+++ Modules/SlashCommands/AdminSlashModule.cs
@@ -1,5 +1,6 @@
 using Discord;
 using Discord.Interactions;
+using Discord.WebSocket;
 using DiscordBot.Attributes;
 using DiscordBot.Services;
 using InteractionFramework.Attributes;
@@ -120,7 +121,6 @@ public class AdminSlashModule : SlashCommandModule{
 
         title = "Moderation Action";
         description = $"**Action Type:** Warning\n\n**Issued by:** {Context.User}#{Context.User.Discriminator}\n\n**Issued to**: {member}#{member.Discriminator}\n\n**Reason:**\n{reason}";
-        color = Discord.Color.Default;
         var modlog = new EmbedBuilder(){
             ThumbnailUrl = thumbnailUrl,
             Title = title,
@@ -139,9 +139,179 @@ public class AdminSlashModule : SlashCommandModule{
 
 
     [SlashCommand("mute", "Mute a member")]
-    public async Task MuteCommandAsync(IUser member, [Summary(description:"Duration in minutes. Defaults to zero (indefinitely).")]int duration = 30, string reason = ""){
-        member = member as IGuildUser ?? throw new Exception("Invalid User");
+    public async Task MuteCommandAsync(IGuildUser member, [Summary(description:"Duration in minutes. Defaults to zero (indefinitely).")]int duration = 0, string reason = ""){
+        //Create a "Muted" role if it does not yet exist
+        string roleName = "Muted";
+        ulong roleId;
+        var role = Context.Guild.Roles.FirstOrDefault(_ => _.Name.Equals(roleName));
+
+        if (role != null)
+            roleId = role.Id;
+        else{
+            var permissions = new GuildPermissions(
+                                            createInstantInvite: false,
+                                            kickMembers: false,
+                                            banMembers: false,
+                                            administrator: false,
+                                            manageChannels: false,
+                                            manageGuild: false,
+                                            addReactions: false,
+                                            viewAuditLog: false,
+                                            viewGuildInsights: false,
+                                            viewChannel: true,
+                                            sendMessages: false,
+                                            manageMessages: false,
+                                            embedLinks: false,
+                                            attachFiles: false,
+                                            readMessageHistory: true,
+                                            mentionEveryone: false,
+                                            useExternalEmojis: false,
+                                            connect: false,
+                                            speak: false,
+                                            muteMembers: false,
+                                            deafenMembers: false,
+                                            moveMembers: false,
+                                            useVoiceActivation: false,
+                                            prioritySpeaker: false,
+                                            stream: false,
+                                            changeNickname: false,
+                                            manageNicknames: false,
+                                            manageRoles: false,
+                                            manageWebhooks: false,
+                                            manageEmojisAndStickers: false,
+                                            useApplicationCommands: false,
+                                            requestToSpeak: false,
+                                            manageEvents: false,
+                                            manageThreads: false,
+                                            createPublicThreads: false,
+                                            createPrivateThreads: false,
+                                            useExternalStickers: false,
+                                            sendMessagesInThreads: false,
+                                            startEmbeddedActivities: false,
+                                            moderateMembers: false,
+                                            useSoundboard: false,
+                                            viewMonetizationAnalytics: false,
+                                            sendVoiceMessages: false,
+                                            useClydeAI: false,
+                                            createGuildExpressions: false,
+                                            setVoiceChannelStatus: false
+            );
+            var newRole = await Context.Guild.CreateRoleAsync(roleName, permissions, color: Discord.Color.Default);
+            roleId = newRole.Id;
+        }
         
-        await RespondAsync("Coming soon!");
+        //Establish the role to add
+        var mutedRole = Context.Guild.GetRole(roleId);
+
+        if (member.RoleIds.Contains(roleId)){
+            await RespondAsync("Member is already muted. If you believe this to be a mistake, check that the \"Muted\" role has the \"Send Messages\" permission turned off.", ephemeral: true);
+            return;
+        }
+
+        //Add role and let the user know
+        await member.AddRoleAsync(mutedRole);
+        await RespondAsync("Member has been muted.", ephemeral: true);
+
+        reason = string.IsNullOrWhiteSpace(reason) ? "No reason given." : reason;
+        var thumbnailUrl = member.GetAvatarUrl();
+        string title = "Mute Notice";
+        string description = $"You have been muted by the {Context.Guild.Name} moderation team.\n\n**Reason:**\n{reason}\n\n**Duration:** {(duration == 0 ? "Unspecified" : $"{duration} minutes")}";
+        var color = Discord.Color.Red;
+
+        var notice = new EmbedBuilder(){
+            ThumbnailUrl = thumbnailUrl,
+            Title = title,
+            Description = description,
+            Color = color,
+        }.WithCurrentTimestamp()
+        .Build();
+
+        await member.SendMessageAsync("", embed: notice);
+
+        //Time the unmute if duration has been specified
+        if (duration > 0){
+            _ = Task.Run(async () => {
+                await Task.Delay(TimeSpan.FromMinutes(duration));
+                await member.RemoveRoleAsync(mutedRole);
+            });
+        }
+
+        //Log the mod action in a modlog channel, if one is set up.
+        var modlogChannelId = _db.GetGuildSettingsAsync(Context.Guild.Id).Result.Modlog;
+        if (string.IsNullOrWhiteSpace(modlogChannelId))
+            return;
+        //ChannelID is passed as a string so we need to convert it to ulong type
+        var modlogChannel = Context.Guild.GetChannel(ulong.Parse(modlogChannelId)) as IMessageChannel;
+        if (modlogChannel == null)
+            return;
+
+        var author = new EmbedAuthorBuilder(){
+                Name = Context.User.GlobalName,
+                IconUrl = Context.User.GetAvatarUrl()
+            };
+        title = "Moderation Action";
+        description = $"**Action Type:** Mute\n\n**Issued by:** {Context.User.Username}\n\n**Issued to**: {member.Username}\n\n**Reason:**\n{reason}\n\n**Duration:** {(duration == 0 ? "Unspecified" : $"{duration} minutes")}";
+        color = Discord.Color.Red;
+        var modlog = new EmbedBuilder(){
+            ThumbnailUrl = thumbnailUrl,
+            Title = title,
+            Description = description,
+            Color = color,
+        }.WithCurrentTimestamp()
+         .Build();
+
+        await modlogChannel.SendMessageAsync("", embed: modlog);
+    }
+
+    [SlashCommand("unmute", "Unmute a member")]
+    public async Task UnmuteCommandAsync(IGuildUser member){
+        string roleName = "Muted";
+        var role = Context.Guild.Roles.FirstOrDefault(_ => _.Name.Equals(roleName));
+
+        if (role == null || !member.RoleIds.Contains(role.Id)){
+            await RespondAsync("This user is not muted, or has not been muted by this bot.");
+            return;
+        }
+
+        await member.RemoveRoleAsync(role);
+
+        string title = "Unmute Notice";
+        string description = $"You have been unmuted by the {Context.Guild.Name} moderation team.";
+        var color = Discord.Color.Default;
+        var notice = new EmbedBuilder(){
+            Title = title,
+            Description = description,
+            Color = color,
+        }.WithCurrentTimestamp()
+        .Build();
+        await member.SendMessageAsync("", embed: notice);
+
+        //Log the mod action in a modlog channel, if one is set up.
+        var modlogChannelId = _db.GetGuildSettingsAsync(Context.Guild.Id).Result.Modlog;
+        if (string.IsNullOrWhiteSpace(modlogChannelId))
+            return;
+        //ChannelID is passed as a string so we need to convert it to ulong type
+        var modlogChannel = Context.Guild.GetChannel(ulong.Parse(modlogChannelId)) as IMessageChannel;
+        if (modlogChannel == null)
+            return;
+
+        var author = new EmbedAuthorBuilder(){
+                Name = Context.User.GlobalName,
+                IconUrl = Context.User.GetAvatarUrl()
+            };
+        var thumbnailUrl = member.GetAvatarUrl();
+        title = "Moderation Action";
+        description = $"**Action Type:** Unmute\n\n**Issued by:** {Context.User.Username}\n\n**Issued to**: {member.Username}";
+        color = Discord.Color.Red;
+        var modlog = new EmbedBuilder(){
+            Author = author,
+            ThumbnailUrl = thumbnailUrl,
+            Title = title,
+            Description = description,
+            Color = color,
+        }.WithCurrentTimestamp()
+         .Build();
+
+        await modlogChannel.SendMessageAsync("", embed: modlog);
     }
 }
